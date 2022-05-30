ca0be27180e54154a90631e0dbd579dd71b2ab3b
AlexSakhno <front-end-san@yandex.ru>
2022-05-30 16:47:50 +0300

    hooks

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,39 +1,33 @@
 import React, { useEffect, useState } from 'react'
 
 const Timer = props => {
-	const [timer, setTimer] = useState(() => ({
-		active: false,
-		timeLeft: null,
-		timerId: null,
-	}))
+	const [active, setActive] = useState(false)
+	const [timeLeft, setTimeLeft] = useState(null)
+	const [timerId, setTimerId] = useState(null)
 
 	useEffect(() => {
-		setTimer(() => ({ timeLeft: props.timer }))
+		setTimeLeft(props.timer)
 	}, [])
 
 	const stepTimer = () => {
-		const { timeLeft } = timer
 		if (!timeLeft) {
 			stopTimer()
 			return
 		}
-		setTimer(({ timeLeft }) => ({ timeLeft: timeLeft - 1 }))
+		setTimeLeft(timeLeft => timeLeft - 1)
 	}
 
 	const playTimer = () => {
-		let { active } = timer
 		if (!active) {
-			let timerId = setInterval(stepTimer, 1000)
-			setTimer(() => ({ ...timer, active: true, timerId: timerId }))
+			setTimerId(setInterval(stepTimer, 1000))
+			setActive(true)
 		}
 	}
 
 	const stopTimer = () => {
-		const { active, timerId } = timer
-
 		if (active) {
 			clearInterval(timerId)
-			setTimer(() => ({ ...timer, active: false, timerId: null }))
+			setActive(false)
 		}
 	}
 
@@ -41,7 +35,6 @@ const Timer = props => {
 		return `0${value}`.slice(-2)
 	}
 
-	const { timeLeft } = timer
 	const minutes = Math.trunc(timeLeft / 60)
 	const seconds = timeLeft % 60
 
